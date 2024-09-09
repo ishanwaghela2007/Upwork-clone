34be623b1e11674b71f830698987f2f9a55a17ef
ishanwaghela2007 <160811456+ishanwaghela2007@users.noreply.github.com>
2024-09-09 16:32:07 +0530

    updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserLayout from './User/Userlayout';
+import Landing from './User/Landingpage/Landing';
 import Home from './User/Pages/Home/Home';
 import Auth from './User/Pages/Auth/Auth';
-
 
 const App = () => {
   return (
     <Router>
-
       <Routes>
-        <Route path='/' element={<UserLayout />} >
-        <Route index element={<Home />} />
-        <Route path='Auth' element={<Auth/>} />
+        <Route path='/' element={<UserLayout />}>
+          <Route index element={<Landing />} />
+          <Route path='Auth' element={<Auth />} />
+          <Route path='home' element={<Home />} />
         </Route>
       </Routes>
-
-
     </Router>
   );
 };
