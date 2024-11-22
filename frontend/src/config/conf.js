42411cf22fd0eacf9b35590d02d2e375e4536a7e
Ishan Waghela <ishanwaghela2007@gmail.com>
2024-11-23 00:08:57 +0530

    Update conf.js

--- frontend/src/config/conf.js
+++ frontend/src/config/conf.js
@@ -1,6 +1,7 @@
-const Conf={
- appwriteURL:string(import.meta.env.VITE_APPWRITE_URL),
- appwriteProjectID:string(import.meta.env.VITE_APPWRITE_PROJECT_ID),
- appwriteDatabaseID:string(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-}
+const Conf = {
+  appwriteURL: String(import.meta.env.VITE_APPWRITE_URL), // Convert to string explicitly
+  appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+};
+
 export default Conf;
